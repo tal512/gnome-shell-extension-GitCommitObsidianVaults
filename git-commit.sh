066cc79dba744f2125659b8bf170f4b57d87920f
tal512 <tal@hunam.net>
2025-05-22 11:37:24 +0300

    Make commit messages more detailed

--- git-commit.sh
+++ git-commit.sh
@@ -10,8 +10,10 @@ git -C "$GIT_PROJECT_PATH" diff --staged --quiet
 if [ $? == 1 ]
 then
   COMMIT_TIME=$(date "+%Y-%m-%d %H:%M:%S")
-  git -C $GIT_PROJECT_PATH commit -m "Update at $COMMIT_TIME"
-  git -C $GIT_PROJECT_PATH push --set-upstream origin main
+  COMMIT_TITLE="Update at $COMMIT_TIME"
+  COMMIT_BODY=$(git -C "$GIT_PROJECT_PATH" diff --staged --stat)
+  git -C "$GIT_PROJECT_PATH" commit -m "$COMMIT_TITLE" -m "$COMMIT_BODY"
+  git -C "$GIT_PROJECT_PATH" push --set-upstream origin main
   notify-send "Changes to Obsidian vaults backed up to GitHub" $GIT_PROJECT_PATH
 else
   notify-send "No changes to Obsidian vaults found"
